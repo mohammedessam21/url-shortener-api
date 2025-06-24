597affd0b12097b52072d7a7cdfa6dc6913a2133
mohammedessam21 <mohammed.essam97@outlook.com>
2025-06-24 18:28:00 +0300

    implement url shortener entity

---
+++ src/url-shortener/entities/url-shortener.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class UrlShortener {
+     @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  originalUrl: string;
+
+  @Column({ unique: true })
+  shortCode: string;
+
+  @Column({ default: 0 })
+  accessCount: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+  
+  @Column({ type: 'timestamp' })
+  expiresAt: Date;
+}
